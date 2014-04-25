87471499ae216f56551effa06fc019efcf2a64a6
Session <wfl211000@163.com>
2014-04-25 12:40:24 +0800

    test1

--- src/com/baihuogou/git/Test.java
+++ src/com/baihuogou/git/Test.java
@@ -2,7 +2,7 @@ package com.baihuogou.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello  World!");
         //vh 
     }
 }
