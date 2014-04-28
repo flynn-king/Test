b6333f2b14246322292d5fa699032b2043f23418
王伏林 <123.com>
2014-04-28 17:00:55 +0800

    sad

--- src/com/baihuogou/git/Test.java
+++ src/com/baihuogou/git/Test.java
@@ -2,12 +2,6 @@ package com.baihuogou.git;
 
 public class Test {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("HelloWorld!");
-=======
-        System.out.println("Hello 123123 World!");
->>>>>>> branch 'master' of ssh://git@github.com/ruanjianyiban/Test.git
-        //vhds
-    
     }
 }
